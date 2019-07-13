7b8991ca434016e17a5228ac740d8d632417ac8d
ferrazf <ferrazf9@gmail.com>
2019-07-13 07:16:44 -0400

    Add new map styles

--- scripts/arcGISMap.js
+++ scripts/arcGISMap.js
@@ -1,10 +1,13 @@
 require([
     "esri/Map",
-    "esri/views/MapView"
-], function (Map, MapView) {
+    "esri/views/MapView",
+    "esri/widgets/BasemapToggle",
+    "esri/widgets/BasemapGallery",
+    "esri/layers/FeatureLayer"
+], function (Map, MapView, BasemapToggle, BasemapGallery, FeatureLayer) {
 
     var map = new Map({
-        basemap: "topo-vector"
+        basemap: "satellite"
     });
 
     var view = new MapView({
@@ -13,4 +16,28 @@ require([
         center: [-79.3850, 43.6480], // longitude, latitude
         zoom: 15
     });
+
+    var basemapToggle = new BasemapToggle({
+        view: view,
+        nextBasemap: "hybrid"
+    });
+
+    var basemapGallery = new BasemapGallery({
+        view: view,
+        source: {
+            portal: {
+                url: "https://www.arcgis.com",
+                useVectorBasemaps: false // Load vector tile basemaps
+            }
+        }
+    });
+
+    // Trailheads feature layer (points)
+    var trailheadsLayer = new FeatureLayer({
+        url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+    });
+
+    map.add(trailheadsLayer);
+    //view.ui.add(basemapToggle, "bottom-right");
+    view.ui.add(basemapGallery, "top-right")
 });
