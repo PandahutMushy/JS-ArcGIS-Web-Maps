18713f53fc0eab94606ab7c73e280bfbcae277f8
ferrazf <ferrazf9@gmail.com>
2019-07-14 00:30:47 -0400

    Add REST API call to fetch precipiation feature layer, Add resources folder and contents

--- scripts/arcGISMap.js
+++ scripts/arcGISMap.js
@@ -3,8 +3,10 @@ require([
     "esri/views/MapView",
     "esri/widgets/BasemapToggle",
     "esri/widgets/BasemapGallery",
-    "esri/layers/FeatureLayer"
-], function (Map, MapView, BasemapToggle, BasemapGallery, FeatureLayer) {
+    "esri/layers/FeatureLayer",
+    "esri/layers/GraphicsLayer",
+    "esri/Graphic"
+], function (Map, MapView, BasemapToggle, BasemapGallery, FeatureLayer, GraphicsLayer, Graphic) {
 
     var map = new Map({
         basemap: "satellite"
@@ -32,12 +34,41 @@ require([
         }
     });
 
-    // Trailheads feature layer (points)
-    var trailheadsLayer = new FeatureLayer({
-        url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+
+    // Reference the feature layer to query
+    var featureLayer = new FeatureLayer({
+        url: "https://services9.arcgis.com/1U6CnmlrcjUh1LpT/arcgis/rest/services/gpm_1d20190712/FeatureServer"
     });
 
-    map.add(trailheadsLayer);
+    // Layer used to draw graphics returned
+    var graphicsLayer = new GraphicsLayer();
+    map.add(graphicsLayer);
+
+    map.add(featureLayer, 0);
     //view.ui.add(basemapToggle, "bottom-right");
-    view.ui.add(basemapGallery, "top-right")
+    view.ui.add(basemapGallery, "top-right");
+
+    function addGraphics(result) {
+        graphicsLayer.removeAll();
+        result.features.forEach(function (feature) {
+            var g = new Graphic({
+                geometry: feature.geometry,
+                attributes: feature.attributes,
+                symbol: {
+                    type: "simple-marker",
+                    color: [0, 0, 0],
+                    outline: {
+                        width: 2,
+                        color: [0, 255, 255],
+                    },
+                    size: "20px"
+                },
+                popupTemplate: {
+                    title: "{TRL_NAME}",
+                    content: "This a {PARK_NAME} trail located in {CITY_JUR}."
+                }
+            });
+            graphicsLayer.add(g);
+        });
+    }
 });
